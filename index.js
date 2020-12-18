0d890f8b6ea87c1b0793d999b122651ef650b693
jdunk <jdunk@users.noreply.github.com>
2020-12-18 02:34:27 -0500

    added creation of event if it doesnt exist

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const getStartOfTomorrow = (offsetInMinutes) => {
 
 let currEvent;
 
-const gcalEventsListProcess = (resp) => {
+const getCurrentEventFromGCalEventsList = (resp, thingName) => {
   console.log(resp.data.items) // All data
   /*
   const events = resp.data.items.map((ev) => ({
@@ -41,103 +41,148 @@ const gcalEventsListProcess = (resp) => {
   console.log(events)
   */
 
+  let currEv = null;
+
   resp.data.items.forEach((ev) => {
     const evNamePieces = ev.summary.split(' ');
 
-    if (evNamePieces.length >= 2 && evNamePieces[1] === config.countedItemName) {
-      currEvent = {
+    if (evNamePieces.length >= 2 && evNamePieces[1] === thingName) {
+      currEv = {
         ...ev,
         count: parseInt(evNamePieces[0], 10) || 0,
       };
     }
   });
+
+  return currEv;
 };
 
-const getCurrEvent = (successCallback) => {
-  currEvent = null;
-
-  gcal.events.list(
-    {
-      calendarId: config.calendarId,
-      timeMin: getStartOfToday(config.timezoneOffsetInMinutes).toISOString(),
-      timeMax: getStartOfTomorrow(config.timezoneOffsetInMinutes).toISOString(),
-      singleEvents: true,
-      orderBy: 'startTime',
-    },
-    (err, resp) => {
-      if (err) {
-        console.log(`The API returned an error: ${err}`)
-        return;
-      }
-
-      gcalEventsListProcess(resp);
-    
-      if (!currEvent) {
-        res.send('no event found for today');
-      }
-
-      successCallback();
-    }
-  );
-};
-
-const addToCurrTotal = (num, successCallback, errorCallback) => {
-  gcal.events.update(
-    {
-      calendarId: config.calendarId,
-      eventId: currEvent.id,
-      resource: {
-        ...currEvent,
-        summary: `${num + currEvent.count} ${config.countedItemName}`,
-        description: !currEvent.description ? num : `${currEvent.description}+${num}`,
+const getCurrEvent = (thingName) => {
+  return new Promise((resolve, reject) => {
+  
+    gcal.events.list(
+      {
+        calendarId: config.calendarId,
+        timeMin: getStartOfToday(config.timezoneOffsetInMinutes).toISOString(),
+        timeMax: getStartOfTomorrow(config.timezoneOffsetInMinutes).toISOString(),
+        singleEvents: true,
+        orderBy: 'startTime',
       },
-    },
-    (err, resp) => {
-      if (err) {
-        errorCallback(`The API returned an error: ${err}`);
-        return;
-      }
+      (err, resp) => {
+        if (err) {
+          reject(new Error(`The API returned an error: ${err}`));
+        }
 
-      successCallback(resp);
-    }
-  );
-};
-
-app.get('/get/:thing', (req, res, next) => {
-  getCurrEvent(() => {
-    res.send({
-      count: currEvent.count
-    });
-  });
-});
-
-app.get('/add/:num/:thing?', (req, res, next) => {
-  getCurrEvent(() => {
-
-    // Validate input
-    const numParamRaw = req.params.num;
-    const numParamTypecast = parseInt(req.params.num, 10);
-
-    if (!numParamTypecast || numParamTypecast < 1) {
-      res.status(400).send(`"${numParamRaw}" is not a valid number to add`);
-      return;
-    }
-
-    addToCurrTotal(
-      numParamTypecast,
-      (resp) => {
-        res.send({
-          count: parseInt(resp.data.summary.substr(0, resp.data.summary.indexOf(' ')), 10),
-          summary: resp.data.summary,
-          description: resp.data.description,
-        });
-        console.log({ resp });
-      },
-      (errMsg) => {
-        res.send(`Error: ${errMsg}`);
+        resolve(getCurrentEventFromGCalEventsList(resp, thingName));
       }
     );
   });
+};
+
+const createGCalEvent = (num, thingName) => {
+  return new Promise((resolve, reject) => {
+    gcal.events.insert(
+      {
+        calendarId: config.calendarId,
+        resource: {
+          start: {
+            date: '2020-12-18',
+          },
+          end: {
+            date: '2020-12-19',
+          },
+          colorId: '11',
+          summary: `${num} ${thingName}`,
+          description: num
+        }
+      },
+      (err, resp) => {
+        if (err) {
+          reject(new Error(`The API returned an error: ${err}`));
+        }
+
+        resolve(resp);
+      }
+    );
+  });
+};
+
+const updateGCalEvent = (eventToUpdate, num, thingName) => {
+  return new Promise((resolve, reject) => {
+    gcal.events.update(
+      {
+        calendarId: config.calendarId,
+        eventId: eventToUpdate.id,
+        resource: {
+          ...eventToUpdate,
+          summary: `${num + eventToUpdate.count} ${thingName}`,
+          description: !eventToUpdate.description ? num : `${eventToUpdate.description}+${num}`,
+        },
+      },
+      (err, resp) => {
+        if (err) {
+          reject(new Error(`The API returned an error: ${err}`));
+        }
+
+        resolve(resp);
+      }
+    );
+  });
+};
+
+const addToCurrTotal = async (currEvent, num, thingName) => {
+  if (!currEvent) {
+    const newEvent = await createGCalEvent(num, thingName);
+    return newEvent;
+  }
+
+  const updatedEvent = await updateGCalEvent(currEvent, num, thingName);
+  return updatedEvent;
+};
+
+app.get('/get/:thing', (req, res, next) => {
+  getCurrEvent(req.params.thing)
+    .then(currEvent => {
+      res.send({
+        count: currEvent && currEvent.count ? currEvent.count : 0
+      });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send(err.toString());
+    });
+});
+
+app.get('/add/:num/:thing', (req, res, next) => {
+  // Validate input
+  const numParamRaw = req.params.num;
+  const numParamTypecast = parseInt(req.params.num, 10);
+
+  if (!numParamTypecast || numParamTypecast < 1) {
+    res.status(400).send(`"${numParamRaw}" is not a valid number to add`);
+    return;
+  }
+
+  getCurrEvent(req.params.thing)
+    .then(currEvent => 
+      addToCurrTotal(
+        currEvent,
+        numParamTypecast,
+        req.params.thing
+      )
+    )
+    .then(resp => {
+      res.send({
+        count: parseInt(resp.data.summary.substr(0, resp.data.summary.indexOf(' ')), 10),
+        summary: resp.data.summary,
+        description: resp.data.description,
+      });
+      console.log({ resp });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send(err.toString());
+    });
 });
 
 app.get('/', (req, res, next) => {
